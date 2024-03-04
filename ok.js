f92f6eadbed098edcf37087403df05123ae7f4a7
786basha <bashamasthan31@gmail.com>
2024-03-04 14:05:02 -0500

    Resume

--- ok.js
+++ ok.js
@@ -1,5 +1,10 @@
 function Resume_mb(){
-    alert("You are redirecting to Resume of 'Basha'")
+    // alert("You are redirecting to Resume of 'Basha'")
     location.href = '/me/Resume.html';
 
 }
+
+const img = document.querySelector("img");
+img.ondragstart = () => {
+  return false;
+};
