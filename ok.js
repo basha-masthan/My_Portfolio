6b8a4815fdff7e47532c9daa8b411d47bc1eca6f
basha-masthan <bashamasthan31@gmail.com>
2024-09-16 15:45:57 +0530

    django

--- ok.js
+++ ok.js
@@ -13,3 +13,7 @@ img.ondragstart = () => {
 function linux_1(){
   window.open('https://www.credly.com/users/masthan-basha-shaik', '_blank').focus();
 }
+
+function pfs(){
+  window.open('https://github.com/basha-masthan/CRT_Group-Project_EDU-Pro', '_blank').focus();
+}
